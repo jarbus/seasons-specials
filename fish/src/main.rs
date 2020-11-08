3dd8b150ac1e987d1479ca08780dbb9f3ca4107d
Darokrithia <dieltabin@gmail.com>
2020-11-08 02:27:27 -0500

    time for daniel to sleep with the fishes

--- fish/src/main.rs
+++ fish/src/main.rs
@@ -1,5 +1,6 @@
 use csv::Reader;
 use csv::Error;
+use std::fs;
 // use serde::Deserialize;
 // use std::io;
 // use std::collections::HashSet;
@@ -7,16 +8,20 @@ use csv::Error;
 
 
 fn main() -> Result<(), Error> {
+
+	// let jsn_file = fs::read_to_string("Locations_To_States.json");
+
+	// let json: serde_json::Value = serde_json::from_str(jsn_file);
+
 	let mut reader = Reader::from_path("All_Regions.csv")?;
 	
     for record in reader.records() {
         let record = record?;
         println!(
-            "In {}, {} built the {} model. It is a {}.",
+            "{} is from {} and is {} available in july",
             &record[0],
             &record[1],
-            &record[2],
-            &record[3]
+            &record[9]
         );
     }
 
