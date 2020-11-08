988bbca819fc40e1332d01191637c8743f6c9ee2
Owen Xie <15828145+nfusionz@users.noreply.github.com>
2020-11-07 22:45:19 -0500

    Fix dependencies and remove autogenerated file

--- fish/target/debug/deps/fish.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tabind\Desktop\RPI\Extra Curiculars\hackrpi2020\seasons-specials\fish\target\debug\deps\fish.exe: src\main.rs
-
-C:\Users\tabind\Desktop\RPI\Extra Curiculars\hackrpi2020\seasons-specials\fish\target\debug\deps\fish.d: src\main.rs
-
-src\main.rs:
