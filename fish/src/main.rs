093f6901dcf168c1098a735bd0834948c5e431a3
jarbus <jarbus@tutanota.com>
2020-11-08 04:15:05 -0500

    removed empty csa

--- fish/src/main.rs
+++ fish/src/main.rs
@@ -6,30 +6,27 @@ use std::fs;
 // use std::collections::HashSet;
 // use std::path::Path;
 
-<<<<<<< Updated upstream
-
-fn main() -> Result<(), Error> {
-
-	// let jsn_file = fs::read_to_string("Locations_To_States.json");
-
-	// let json: serde_json::Value = serde_json::from_str(jsn_file);
-
-	let mut reader = Reader::from_path("All_Regions.csv")?;
-	
-    for record in reader.records() {
-        let record = record?;
-        println!(
-            "{} is from {} and is {} available in july",
-            &record[0],
-            &record[1],
-            &record[9]
-        );
-    }
-
-    Ok(())
-}
-=======
-fn example() -> Result<(), Box<dyn Error>> {
+//fn main() -> Result<(), Error> {
+//
+//	// let jsn_file = fs::read_to_string("Locations_To_States.json");
+//
+//	// let json: serde_json::Value = serde_json::from_str(jsn_file);
+//
+//	let mut reader = Reader::from_path("All_Regions.csv")?;
+//
+//    for record in reader.records() {
+//        let record = record?;
+//        println!(
+//            "{} is from {} and is {} available in july",
+//            &record[0],
+//            &record[1],
+//            &record[9]
+//        );
+//    }
+//
+//    Ok(())
+//}
+fn example() -> Result<Ok,Err> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = Reader::from_reader("../../seafood/Alaska");
     for result in rdr.records() {
@@ -42,10 +39,9 @@ fn example() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() -> io::Reader<()> {
-    f let Err(err) = example() {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
 	Ok(());
+    let f = match example(){
+        Ok(str) => str
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
->>>>>>> Stashed changes
