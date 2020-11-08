f86e66adc5f56d733c7b2ca83fc4c92bd99a258d
Owen Xie <15828145+nfusionz@users.noreply.github.com>
2020-11-07 22:29:28 -0500

    Some refactoring

---
+++ server/src/routes.rs
@@ -0,0 +1,28 @@
+use rocket::State;
+
+use crate::location::Csa;
+
+pub struct TempState {
+    pub csas: Vec<Csa>,
+}
+
+#[get("/data/csa/<id>")]
+pub fn csa(id: String, state: State<TempState>) -> String {
+    let produce = state.csas
+        .iter()
+        .filter(|csa| csa.uri_eq(&id))
+        .nth(0);
+
+    match produce {
+        Some(c) => {
+            let produce_fmt = c
+                .get_products()
+                .iter()
+                .map(|product| format!("{}: {}", product.get_name(), product.get_category()))
+                .collect::<Vec<String>>()
+                .join("\n");
+            format!("Name of CSA: {}\n{}", id, produce_fmt)
+        },
+        None => String::from("Error: CSA not found.")
+    }
+}
