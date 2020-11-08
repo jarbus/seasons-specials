bf36d94d3e313c5c2f89d344958453b784846998
Darokrithia <dieltabin@gmail.com>
2020-11-08 01:56:00 -0500

    I get rust maybe?

--- fish/src/main.rs
+++ fish/src/main.rs
@@ -1,32 +1,24 @@
 use csv::Reader;
-use serde::Deserialize;
-use std::io;
-use std::collections::HashSet;
-use std::path::Path;
+use csv::Error;
+// use serde::Deserialize;
+// use std::io;
+// use std::collections::HashSet;
+// use std::path::Path;
 
-#[derive(Clone, Debug, Deserialize)]
-struct LifeExpectancy {
-    #[serde(rename = "location")]
-    country: String,
-    life_expectancy: Option<f32>,
-}
 
-fn main() -> io::Result<()> {
-	println!("1");
-    let mut reader = Reader::from_path(Path::new("./All_Regions.csv"))?;
-	println!("2");
-    let mut filter = HashSet::new();
-    let mut count = 0;
-
-    for record in reader.deserialize() {
-        let record: LifeExpectancy = record?;
-        if !filter.contains(&record.country) {
-            println!("{:?}", record);
-            filter.insert(record.country);
-            count += 1;
-        }
+fn main() -> Result<(), Error> {
+	let mut reader = Reader::from_path("All_Regions.csv")?;
+	
+    for record in reader.records() {
+        let record = record?;
+        println!(
+            "In {}, {} built the {} model. It is a {}.",
+            &record[0],
+            &record[1],
+            &record[2],
+            &record[3]
+        );
     }
 
-    println!("{} countries", count);
     Ok(())
 }
